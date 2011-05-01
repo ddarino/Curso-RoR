349e7b3ddaea9c795a0e6d1efd5762a8bfa13407
Damian Darino <ddarino@gmail.com>
2011-05-01 17:12:51 -0300

    Mi primer app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabancaAds::Application.config.secret_token = '20a5b061d88aadacae24e490296716a7416b7e17168329d01350710eeada452a031944c5bbd2f31267c0c89347b8fe5d0170930fa45c398df54793998ef611a4'
