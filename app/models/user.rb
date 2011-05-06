f8362e1d155dac53e1b6e5f090acb00d6038c956
ddarino@gmail.com <damian.darino@labanca.com.uy>
2011-05-06 11:57:15 -0300

    Asociacion entre usuario y advertisements

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+
+  #validates_presence_of :first_name 
+  validates :first_name, :presence => true # igual anterior pero en rails 3
+
+  has_many :advertisements
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject {User.new(:email => "pepe@gmail.com", :password => "pepepe", :password_confirmation => "pepepe")}
+	describe 'validations' do
+		it 'should validates the presence of first_name' do
+			#pepe = User.new
+			#pepe.valid?
+			subject.should_not be_valid
+
+			subject.errors.should include(:first_name)
+		end
+	end
 end
